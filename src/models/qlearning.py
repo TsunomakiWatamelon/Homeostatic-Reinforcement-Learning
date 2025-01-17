4853ee9a11ed75b49c827a98e561781d4cf58216
HERVE NGUYEN <83554610+TsunomakiWatamelon@users.noreply.github.com>
2025-01-17 17:11:31 +0100

    Replaced epsilon greedy from q-learning to Softmax rule

--- src/models/qlearning.py
+++ src/models/qlearning.py
@@ -2,7 +2,7 @@ import numpy as np
 import copy
 
 class QLearning:
-    def __init__(self, state_size, action_size, alpha=0.1, gamma=0.99, epsilon=0.1, q_table=None):
+    def __init__(self, state_size, action_size, alpha=0.1, gamma=0.99, temperature=0.1, q_table=None):
         """
         Implémente un agent Q-learning.
         """
@@ -10,35 +10,39 @@ class QLearning:
         self.action_size = action_size
         self.alpha = alpha  # Taux d'apprentissage
         self.gamma = gamma  # Facteur d'actualisation
-        self.epsilon = epsilon  # Probabilité d'exploration
+        self.temperature = temperature # Température pour la règle softmax
 
         # Initialisation de la table Q avec des zéros
-        if q_table is not None:
+        if q_table is not None and type(q_table) == dict:
             self.q_table = copy.deepcopy(q_table)
+            self.is_dict = True
         else:
             self.q_table = np.zeros((state_size, action_size))
+            self.is_dict = False
 
     def choose_action(self, state):
         """
-        Choisit une action en fonction de la stratégie epsilon-greedy.
+        Choisit une action en fonction de la règle softmax.
         :param state: État courant.
         :return: Action choisie (entier entre 0 et action_size - 1).
         """
         # Vérification de l'existence de l'état dans la table Q
-        if state not in self.q_table:
+        if self.is_dict and state not in self.q_table:
             raise KeyError(f"L'état {state} n'existe pas dans la table Q.")
 
         # Vérification que des actions sont disponibles
-        if len(self.q_table[state]) == 0:
+        if not self.is_dict and len(self.q_table[state]) == 0:
             raise ValueError(f"Pas d'actions disponibles pour l'état {state}.")
 
-        # Stratégie epsilon-greedy
-        if np.random.rand() < self.epsilon:
-            # Exploration : choisir une action aléatoire
-            return np.random.randint(len(self.q_table[state]))
-        else:
-            # Exploitation : choisir l'action avec la plus grande valeur Q
-            return np.argmax(self.q_table[state])
+        q_values = self.q_table[state]
+
+        # Appliquer la règle softmax pour calculer les probabilités
+        tau = self.temperature  # Température pour la règle softmax
+        exp_q_values = np.exp(q_values / tau)  # Exponentiation des valeurs Q
+        probabilities = exp_q_values / np.sum(exp_q_values)  # Distribution des probabilités
+
+        # Choisir une action en fonction des probabilités calculées
+        return np.random.choice(len(q_values), p=probabilities)
 
 
     def update_q_value(self, state, action, reward, next_state):
