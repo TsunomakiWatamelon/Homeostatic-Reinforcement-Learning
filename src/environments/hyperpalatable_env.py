ad03b636b7167b31e4da8efbe82d907d9b9da9a2
h2ocine <157651604+h2ocine@users.noreply.github.com>
2025-01-21 17:16:21 +0100

    hyperpalatable experiment

--- src/environments/hyperpalatable_env.py
+++ src/environments/hyperpalatable_env.py
@@ -1,7 +1,7 @@
 from environments.base_env import HomeostaticEnvironment
 
 class HyperpalatableEnvironment(HomeostaticEnvironment):
-    def __init__(self, H, setpoints, weights, exponents, effects, palatability_bonus, normal_food_reward, threshold):
+    def __init__(self, H, setpoints, weights, exponents, effects, palatability_bonus, normal_food_reward, energy_cost, no_normal_food = False, no_tatsy_food = False):
         """
         Initialise l'environnement pour l'expérience de surconsommation de nourriture hyperpalatable.
         Hérite de HomeostaticEnvironment.
@@ -10,20 +10,33 @@ class HyperpalatableEnvironment(HomeostaticEnvironment):
         self.strating_energy = H[0]
         self.palatability_bonus = palatability_bonus  # Bonus pour nourriture hyperpalatable (T)
         self.normal_food_reward = normal_food_reward  # Récompense pour nourriture normale
-        self.threshold = threshold  # Seuil pour terminer l'épisode
+        self.energy_cost = energy_cost # L'energie consomé a chaque step
+
+        self.no_normal_food = no_normal_food # can choose between hyperpalatable food and nothing
+        self.no_tasty_food = no_tatsy_food # can only choose between normal food and nothing (nul)
 
     def step(self, action):
         """
         Effectue une étape dans l'environnement en prenant une action.
-        Calcule la récompense selon la formule donnée et met à jour l'état.
         """
-        # Modifier l'état interne en fonction de l'action
-        if action == 0:  # Nourriture normale
-            self.state[0] += self.normal_food_reward
-        elif action == 1:  # Nourriture hyperpalatable
-            self.state[0] += self.normal_food_reward + self.palatability_bonus
+        # Definir l'action choisit
+        if not self.no_normal_food and not self.no_tasty_food:
+            normal = action == 0
+            hyper = action == 1
+        elif self.no_tasty_food:
+            normal = action == 0
+            hyper = False
         else:
-            raise ValueError(f"Action inconnue : {action}")
+            normal = False
+            hyper = action == 0
+        
+        # Modifier l'état interne 
+        self.state[0] -= self.energy_cost # A chaque step il consomme energy cost energie
+
+        if normal:  # Nourriture normale
+            self.state[0] += self.normal_food_reward
+        elif hyper == 1:  # Nourriture hyperpalatable
+            self.state[0] += self.normal_food_reward + self.palatability_bonus
 
         # Calcule de la récompense 
         new_state = self.state[0]
@@ -32,15 +45,18 @@ class HyperpalatableEnvironment(HomeostaticEnvironment):
         # Mise à jour de l'état interne dans l'objet Drive
         self.drive.update_state(self.state)
 
-        # Vérifier le seuil pour terminer l'épisode est atteint
-        done = self.state[0] > self.threshold
-
         # Retourner l'état, la récompense totale et le signal de fin d'épisode
         base_state = 0 # On a un seul état dans cette experience
-        return base_state, reward, done, {}
+        return base_state, reward, False, {}
 
     def reset(self):
         state = 0  # On a un seul etats
         self.state = [self.strating_energy] # Revenir au niveau d'energie de base
         self.drive.update_state(self.state)
         return state
+    
+    def set_no_normal_food(self,no_normal_food=True):
+        self.no_normal_food = no_normal_food
+
+    def set_no_tasty_food(self,no_tatsy_food=True):
+        self.no_tasty_food = no_tatsy_food
